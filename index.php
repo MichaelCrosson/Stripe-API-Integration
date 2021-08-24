3d63a838cce6493d9c8c6279940db049af6c51e5
Michael <70046213+MichaelCrosson@users.noreply.github.com>
2021-08-24 10:35:32 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once('vendor/autoload.php');
+\Stripe\Stripe::setApiKey('sk_test_51J3cvIGKUmLV3VlznAGQprgn6DpjvqcRyNcsZsxoWNu$
+
+$session = \Stripe\Checkout\Session::create([
+  'payment_method_types' => ['card'],
+  'line_items' => [[
+    'price_data' => [
+      'currency' => 'usd',
+      'product_data' => [
+        'name' => 'INSERT NAME',
+      ],
+      'unit_amount' => 100,
+    ],
+    'quantity' => 1,
+  ]],
+  'mode' => 'payment',
+  'success_url' => 'http://jesusfilled.com/success.html',
+    'cancel_url' => 'http://jesusfilled.com/cancel.html',
+]);
+
+?>
+
+<html>
+  <head>
+    <title>JesusFilled</title>
+    <script src="https://js.stripe.com/v3/"></script>
+  </head>
+  <body>
+    <button id="checkout-button">Checkout</button>
+    <script>
+      var stripe =
+Stripe('pk_test_51J3cvIGKUmLV3Vlzw5tJn6sHKFgxzBZsQbnb3iaPpeLVBQqyjvbKg9APRqju9A$
+      const btn = document.getElementById("checkout-button")
+      btn.addEventListener('click', function(e) {
+      e.preventDefault();
+      stripe.redirectToCheckout({
+          sessionId: "<?php echo $session->id; ?>"
+        });
+      });
+    </script>
+  </body>
+</html>
+
+
